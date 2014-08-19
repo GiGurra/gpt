11f9d37e58d99b034a514f6fa82261e4123040b8
Johan Kjölhede <kjolhede@telia.com>
2014-08-19 16:56:43 +0200

    fixed names

--- gpt-common/src/main/scala/se/gigurra/gpt/common/NetworkNames.scala
+++ gpt-common/src/main/scala/se/gigurra/gpt/common/NetworkNames.scala
@@ -2,9 +2,9 @@ package se.gigurra.gpt.common
 
 object NetworkNames {
   val SHM_TRANSMITTER = "gpt-shm-transmitter"
-  val SHM_RECEIVER = "gpt-shm-transmitter"
+  val SHM_RECEIVER = "gpt-shm-receiver"
   val KEY_TRANSMITTER = "gpt-key-transmitter"
-  val KEY_RECEIVER = "gpt-key-transmitter"
+  val KEY_RECEIVER = "gpt-key-receiver"
   val DISP_TRANSMITTER = "gpt-disp-transmitter"
-  val DISP_RECEIVER = "gpt-disp-transmitter"
+  val DISP_RECEIVER = "gpt-disp-receiver"
 }
