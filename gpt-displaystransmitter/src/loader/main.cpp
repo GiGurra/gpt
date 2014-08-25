1ab97ec96c6778aac9d96156cd424c32442c0ebe
Johan Kjölhede <kjolhede@telia.com>
2014-08-25 18:31:19 +0200

    Update main.cpp

--- gpt-displaystransmitter/src/loader/main.cpp
+++ gpt-displaystransmitter/src/loader/main.cpp
@@ -8,9 +8,8 @@ int main(int argc, char *argv[]) {
 	PROCESS_INFORMATION p = { 0 };
 
 	std::stringstream stream;
-	for (int i = 0; i < argc; i++) {
+	for (int i = 0; i < argc; i++)
 		stream << " " << argv[i];
-	}
 	std::string args = stream.str();
 	
 	return !DetourCreateProcessWithDll("Falcon BMS.exe", (char*)(args.c_str()), NULL, NULL, TRUE, 0, NULL, NULL, &s, &p, "gpt_displaystransmitter_hook.dll", 0);
