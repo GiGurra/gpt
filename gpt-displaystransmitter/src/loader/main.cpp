2eac7ff777e3ba20b97892e521b4a6b684823afa
Johan Kjölhede <kjolhede@telia.com>
2014-08-25 17:10:01 +0200

    Adding support for command line parameters

--- gpt-displaystransmitter/src/loader/main.cpp
+++ gpt-displaystransmitter/src/loader/main.cpp
@@ -1,9 +1,18 @@
 #pragma comment(lib,"detours.lib")
 #include "windows.h"
 #include "detours.h"
+#include <sstream>
 
-int main() {
+int main(int argc, char *argv[]) {
 	STARTUPINFO s = { sizeof(STARTUPINFO) };
 	PROCESS_INFORMATION p = { 0 };
-	return !DetourCreateProcessWithDll("Falcon BMS.exe", NULL, NULL, NULL, TRUE, 0, NULL, NULL, &s, &p, "gpt_displaystransmitter_hook.dll", 0);
+
+	std::stringstream stream;
+	for (int i = 0; i < argc; i++) {
+		stream << " ";
+		stream << argv[i];
+	}
+	std::string args = stream.str().append("\0");
+	
+	return !DetourCreateProcessWithDll("Falcon BMS.exe", &args[0], NULL, NULL, TRUE, 0, NULL, NULL, &s, &p, "gpt_displaystransmitter_hook.dll", 0);
 }
